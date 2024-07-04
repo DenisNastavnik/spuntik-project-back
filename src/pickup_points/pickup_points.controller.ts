2ebde36eecacff70f95671ec2d45541dd7e6cb3c
Andrey <dimitriev01@list.ru>
2024-07-04 13:50:31 +0700

    feature: enpoint pickup points

--- src/pickup_points/pickup_points.controller.ts
+++ src/pickup_points/pickup_points.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { PickupPointsService } from './pickup_points.service';
+import { PickupPoint } from './pickup_points.schema';
 
 @Controller('pickup-points')
-export class PickupPointsController {}
+export class PickupPointsController {
+  constructor(private readonly pickupPointsService: PickupPointsService) {}
+
+  @ApiOperation({ summary: 'Получение списка всех точек выдачи' })
+  @ApiResponse({ status: 200, type: [PickupPoint] })
+  @Get()
+  async findAll(): Promise<PickupPoint[]> {
+    try {
+      return await this.pickupPointsService.findAll();
+    } catch (error) {
+      throw new HttpException(
+        'Ошибка при получении точек выдачи',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
