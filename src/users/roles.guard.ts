3d2888050aca4234073a993563a52e1e86dd3ab7
Veronika <veronika.korolkova99@gmail.com>
2024-07-05 23:20:22 +0700

    feature: Created guard for roles.

---
+++ src/users/roles.guard.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorators/roles.decorators';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return false;
+    }
+
+    const token = authHeader.split(' ')[1];
+    const decoded = this.jwtService.verify(token);
+    const userRoles = decoded.roles;
+    const hasRole = requiredRoles.some((role) => userRoles.includes(role));
+    if (!hasRole) {
+      throw new ForbiddenException('Отказано в праве доступа!');
+    }
+    return true;
+  }
+}
