839e3813486b335923f0e7f4ad968f966522e708
Andrey Dimitriev <dimitriev01@list.ru>
2024-08-09 18:37:10 +0700

    feature: role vendor for file-upload, get products by ids

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -6,21 +6,28 @@ import {
   UploadedFiles,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor, FileFieldsInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 import { BufferedFile } from 'src/minio-client/minio-client.model';
+import { AuthGuard, RolesGuard } from 'src/users';
+import { Role, Roles } from 'src/decorators';
 
 @Controller('file-upload')
 export class FileUploadController {
   constructor(private fileUploadService: FileUploadService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Vendor)
   @Post('single')
   @UseInterceptors(FileInterceptor('image'))
   async uploadSingle(@UploadedFile() file: BufferedFile) {
     return await this.fileUploadService.uploadSingle(file);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Vendor)
   @Post('many')
   @UseInterceptors(
     FileFieldsInterceptor([
@@ -32,6 +39,8 @@ export class FileUploadController {
     return this.fileUploadService.uploadMany(files);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.Vendor)
   @Delete(':fileName')
   async deleteSingle(@Param('fileName') fileName: string) {
     return await this.fileUploadService.deleteFile(fileName);
